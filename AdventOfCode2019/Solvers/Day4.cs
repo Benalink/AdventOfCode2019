64c8df7dbd3ff84513b83e4ba90bebc19738dea8
Ben Alink <benalink@live.co.uk>
2019-12-06 12:17:35 +0000

    Day 4 complete

---
+++ AdventOfCode2019/Solvers/Day4.cs
@@ -0,0 +1,93 @@
+﻿namespace AdventOfCode2019.Solvers
+{
+    internal static partial class Solvers
+    {
+        internal static class Day4
+        {
+            internal static int Part1(int min, int max)
+            {
+                int valid = 0;
+
+                for (int i = min; i < max; i++)
+                {
+                    char[] candidate = i.ToString().ToCharArray();
+                    char lastSeen = candidate[0];
+                    bool hasADouble = false;
+                    bool neverDecreases = true;
+
+                    for (int j = 1; j < candidate.Length; j++)
+                    {
+                        if (candidate[j] == lastSeen)
+                        {
+                            hasADouble = true;
+                        }
+
+                        if (int.Parse(candidate[j].ToString()) < int.Parse(lastSeen.ToString()))
+                        {
+                            neverDecreases = false;
+                        }
+
+                        lastSeen = candidate[j];
+                    }
+
+                    if (hasADouble && neverDecreases)
+                    {
+                        valid++;
+                    }
+                }
+
+                return valid;
+            }
+
+            internal static int Part2(int min, int max)
+            {
+                int valid = 0;
+
+                for (int i = min; i < max; i++)
+                {
+                    char[] candidate = i.ToString().ToCharArray();
+                    char lastSeen = candidate[0];
+                    int repeated = 0;
+                    bool hasADoubleStrict = false;
+                    bool neverDecreases = true;
+
+                    for (int j = 1; j < candidate.Length; j++)
+                    {
+                        if (lastSeen == candidate[j])
+                        {
+                            repeated++;
+                        }
+                        else
+                        {
+                            if (repeated == 1)
+                            {
+                                hasADoubleStrict = true;
+                            }
+
+                            repeated = 0;
+                        }
+
+                        if (int.Parse(candidate[j].ToString()) < int.Parse(lastSeen.ToString()))
+                        {
+                            neverDecreases = false;
+                        }
+                        
+                        lastSeen = candidate[j];
+                    }
+
+                    if (repeated == 1)
+                    {
+                        hasADoubleStrict = true;
+                    }
+
+                    if (hasADoubleStrict && neverDecreases)
+                    {
+                        valid++;
+                    }
+                }
+
+                return valid;
+            }
+        }
+    }
+}
---
+++ AdventOfCode2019Tests/Day4Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2019.Solvers;
+using Xunit;
+
+namespace AdventOfCode2019Tests
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void Part1SolvesCorrectly()
+        {
+            int validPasswords = Solvers.Day4.Part1(134792, 675810);
+            Assert.Equal(1955, validPasswords);
+        }
+
+        [Fact]
+        public void Part2SolvesCorrectly()
+        {
+            int validPasswords = Solvers.Day4.Part2(134792, 675810);
+            Assert.Equal(1319, validPasswords);
+
+        }
+    }
+}
